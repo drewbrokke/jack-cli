c53efd6e32ca769abefcdb4f375360bee575ced9
drewbrokke <drew.brokke@liferay.com>
2017-05-31 08:52:35 -0700

    2 - moves types to its own dir

--- src/types/types.ts
+++ src/types/types.ts
@@ -14,3 +14,29 @@ export interface IListElement extends Blessed.Widgets.ListElement {
 	pushItem(child: BlessedElement | string): number;
 	setItems(items: BlessedElement[] | string[]): void;
 }
+
+export type View = 'COMMIT' | 'LIST';
+
+export type ActionType =
+	'ADD_COMMITS' |
+	'DECREMENT_INDEX' |
+	'INCREMENT_INDEX' |
+	'NEXT_COMMIT' |
+	'PREVIOUS_COMMIT' |
+	'SET_COMMIT_CONTENT' |
+	'TOGGLE_SPLIT' |
+	'VIEW_COMMIT' |
+	'VIEW_LIST';
+
+export interface IState {
+	view: View;
+	split: boolean;
+	index: number;
+	commits: string[];
+	content: string;
+}
+
+export interface IAction {
+	type: ActionType;
+	payload?: any;
+}
