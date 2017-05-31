c7681064630f747584214cc436bc0fe608f1d08f
drewbrokke <drew.brokke@liferay.com>
2017-05-31 08:26:00 -0700

    updates build script for slightly more informative building

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 echo "Transpiling Typescript files..."
 echo ""
 
-tsc --pretty
+tsc --pretty && echo "No build errors!"
 
 echo ""
 echo "Linting Typescript files..."
 echo ""
 
-tslint -c tslint.json -p tsconfig.json -t stylish
+tslint -c tslint.json -p tsconfig.json -t stylish && echo "No lint errors!"
 
 echo ""
