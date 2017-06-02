6af0500f520e2591fb35c63d0bf889d5ff139d4b
drewbrokke <drew.brokke@liferay.com>
2017-06-02 08:16:09 -0700

    interface-elements.ts - exports getListElement function and allows passing of options

--- src/interface/interface-elements.ts
+++ src/interface/interface-elements.ts
@@ -3,7 +3,7 @@ import {
 	BoxElement,
 	BoxOptions,
 	IListElement,
-	KeyEvent,
+	ListOptions,
 	Screen,
 	ScreenOptions,
 	ScrollableTextElement,
@@ -16,14 +16,8 @@ export function getBoxElement(options: BoxOptions): BoxElement {
 	return Blessed.box(options);
 }
 
-export function getCommitListElement(
-	onKeyPressFn: (ch: string, key: KeyEvent) => void): IListElement {
-
-	const list: IListElement = getListElement();
-
-	list.on('keypress', onKeyPressFn);
-
-	return list;
+export function getListElement(options: ListOptions): IListElement {
+	return Blessed.list(options);
 }
 
 export function getScrollableTextElement(
@@ -38,26 +32,4 @@ export function getScreenElement(options: ScreenOptions): Screen {
 
 export function getTextElement(options: TextOptions): TextElement {
 	return Blessed.text(options);
-}
-
-/*
-Helper Functions
-*/
-
-function getListElement(): IListElement {
-	const listBgColor: string = '#555';
-
-	return Blessed.list({
-		bottom: 0,
-		left: 0,
-		mouse: true,
-		right: 0,
-		style: {
-			selected: {
-				bg: listBgColor,
-			},
-		},
-		tags: true,
-		top: 0,
-	});
 }
