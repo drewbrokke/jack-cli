575f9bb12c7d6bb5446714a7f77608cb0b957b25
drewbrokke <drew.brokke@liferay.com>
2017-06-02 00:28:42 -0700

    screen.ts - copies SHA to clipboard on 'c' keypress

--- src/interface/screen.ts
+++ src/interface/screen.ts
@@ -1,3 +1,7 @@
+import * as clipboardy from 'clipboardy';
+
+import { notificationRequested } from '../redux/action-creators';
+import { store } from '../redux/store';
 import { Screen } from '../types/types';
 import { getScreenElement } from './interface-elements';
 
@@ -6,7 +10,16 @@ export function getScreen(): Screen {
 		smartCSR: true,
 	});
 
+	screen.key('c', copySHAToClipboard);
 	screen.key(['C-c', 'q', 'escape'], () => process.exit(0));
 
 	return screen;
+}
+
+function copySHAToClipboard(): void {
+	const {SHA} = store.getState();
+
+	clipboardy.writeSync(SHA);
+
+	store.dispatch(notificationRequested(`Copied SHA to the clipboard: ${SHA}`));
 }
