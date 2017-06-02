0218c1356ede5f379d01df625aa02afa2cf95d4e
drewbrokke <drew.brokke@liferay.com>
2017-06-02 11:38:24 -0700

    adds help-text.ts

---
+++ src/util/help-text.ts
@@ -0,0 +1,30 @@
+export const helpText = `
+Global Key Commands:
+- Exit jack:                                q | esc | ctrl + c
+- Show/hide this help dialog:               ?
+
+- Cherry-pick commit to current branch:     c
+- Open changed files in default editor:     o
+- Copy current commit's SHA to clipboard:   y
+
+Navigation Key Commands:
+
+List view
+- Select next item:                         down | j
+- Select previous item:                     up | k
+- Go to Commit View (git show the commit):  space | enter
+
+Commit view
+- View next commit:                         shift + down | shift + j | right
+- View previous commit:                     shift + up | shift + k | left
+- Return to List View:                      space | enter
+- Navigating commit content:
+    + Scroll up:                            up | k
+    + Scroll down:                          down | j
+    + Scroll up half screen:                ctrl + u
+    + Scroll down half screen:              ctrl + d
+    + Scroll up full screen:                ctrl + b
+    + Scroll down full screen:              ctrl + f
+    + Scroll to top:                        g
+    + Scroll to bottom:                     shift + g
+`;
