54632d1899e3c13ddb5e01fc8fd928bcd31acc58
drewbrokke <drew.brokke@liferay.com>
2017-06-02 00:30:57 -0700

    build

--- dist/interface/screen.js
+++ dist/interface/screen.js
@@ -1,11 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const clipboardy = require("clipboardy");
+const action_creators_1 = require("../redux/action-creators");
+const store_1 = require("../redux/store");
 const interface_elements_1 = require("./interface-elements");
 function getScreen() {
     const screen = interface_elements_1.getScreenElement({
         smartCSR: true,
     });
+    screen.key('c', copySHAToClipboard);
     screen.key(['C-c', 'q', 'escape'], () => process.exit(0));
     return screen;
 }
 exports.getScreen = getScreen;
+function copySHAToClipboard() {
+    const { SHA } = store_1.store.getState();
+    clipboardy.writeSync(SHA);
+    store_1.store.dispatch(action_creators_1.notificationRequested(`Copied SHA to the clipboard: ${SHA}`));
+}
