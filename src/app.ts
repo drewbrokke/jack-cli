1face3530ec6c6b5711e94c7de939e25d2a229dc
drewbrokke <drew.brokke@liferay.com>
2017-06-04 01:12:32 -0700

    app.ts - inline spawn process

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
-import { ChildProcess } from 'child_process';
+import { ChildProcess, spawn } from 'child_process';
 
 import { getScreen } from './interface/screen';
 import { addCommits } from './redux/action-creators';
 import { store } from './redux/store';
 
-import { getGitLogProcess } from './util/git-util';
-
 export function run(args: string[]): void {
 	getScreen().render();
 
-	const gitLogProcess: ChildProcess = getGitLogProcess(args);
+	const gitLogProcess: ChildProcess = spawn(
+		'git',
+		[
+			'log',
+			'--color=always',
+			...args,
+		],
+	);
 
 	gitLogProcess.stdout.setEncoding('utf8');
 	gitLogProcess.stdout.on('data', (data: string) => {
