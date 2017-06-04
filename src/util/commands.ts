f4597d78bb38697e853e49bad8cc09105b58e66c
drewbrokke <drew.brokke@liferay.com>
2017-06-04 01:05:38 -0700

    commands.ts - uses all asynchronous calls

--- src/util/commands.ts
+++ src/util/commands.ts
@@ -1,22 +1,19 @@
-import { ChildProcess, exec, spawn, spawnSync, SpawnSyncReturns } from 'child_process';
+import { ChildProcess, spawn } from 'child_process';
 import * as clipboardy from 'clipboardy';
 import * as opn from 'opn';
 import * as path from 'path';
 
 import { notifyError, notifyInfo, notifySuccess } from '../interface/notification';
 
-const REPO_TOP_LEVEL: string = spawnSync('git', ['rev-parse', '--show-toplevel']).stdout.toString().split('\n')[0];
+let REPO_TOP_LEVEL: string;
 
 export function cherryPickCommit(SHA: string): void {
-	const cherryPickSync: SpawnSyncReturns<string> = spawnSync('git', ['cherry-pick', SHA]);
-
-	if (cherryPickSync.status !== 0) {
-		notifyError(`Cherry-pick failed:\n\n${cherryPickSync.stderr.toString()}\n\nAborting cherry-pick.`);
-
-		spawn('git', ['cherry-pick', '--abort']);
-	} else {
-		notifySuccess(`Successfully cherry-picked commit ${SHA} onto current branch.`);
-	}
+	promisifyChildProcess(spawn('git', ['cherry-pick', SHA]))
+		.then(() => notifySuccess(`Successfully cherry-picked commit ${SHA} onto current branch.`))
+		.catch((errorMessage: string) => {
+			notifyError(`Cherry-pick failed:\n\n${errorMessage}\n\nAborting cherry-pick.`);
+			spawn('git', ['cherry-pick', '--abort']);
+		});
 }
 
 export function copySHAToClipboard(SHA: string): void {
@@ -25,29 +22,53 @@ export function copySHAToClipboard(SHA: string): void {
 }
 
 export function openFilesFromCommit(SHA: string): void {
-	exec(`git diff --name-only ${SHA}^..${SHA}`, (_error: Error, stdout: string) => {
-		const files: string[] = stdout.split('\n').filter(Boolean);
+	if (REPO_TOP_LEVEL) {
+		doOpenFilesFromCommit(SHA, REPO_TOP_LEVEL);
+	} else {
+		promisifyChildProcess(spawn('git', ['rev-parse', '--show-toplevel']))
+			.then((repoTopLevel: string) => {
+				REPO_TOP_LEVEL = repoTopLevel;
 
-		files.map((file: string) => path.join(REPO_TOP_LEVEL, file)).forEach(opn);
+				doOpenFilesFromCommit(SHA, repoTopLevel);
+			})
+			.catch(handleOpenFilesFromCommitError);
+	}
+}
 
-		notifyInfo(`Opening files:\n\n${files.join('\n')}`);
-	});
+// Helper functions
+
+function doOpenFilesFromCommit(SHA: string, repoTopLevel: string): void {
+	promisifyChildProcess(spawn('git', ['diff', '--name-only', `${SHA}^..${SHA}`]))
+		.then((filesString: string) => {
+			const files: string[] = filesString.split('\n');
+
+			files.map((file: string) => path.join(repoTopLevel, file)).forEach(opn);
+
+			notifyInfo(`Opening files:\n\n${filesString}`);
+		})
+		.catch(handleOpenFilesFromCommitError);
+}
+
+function handleOpenFilesFromCommitError(errorMessage: string) {
+	notifyError(`Could not open files:\n\n${errorMessage}`);
 }
 
 function promisifyChildProcess(childProcess: ChildProcess): Promise<any> {
 	return new Promise((resolve, reject) => {
 		let dataString = '';
+		let errorString = '';
 
 		childProcess.stdout.setEncoding('utf8');
+		childProcess.stderr.setEncoding('utf8');
 
 		childProcess.stdout.on('data', (data: string) => dataString += data);
-		childProcess.stderr.on('error', (error: Error) => reject(error));
+		childProcess.stderr.on('data', (data: string) => errorString += data);
 
 		childProcess.on('close', (code: number) => {
 			if (code === 0) {
-				resolve(dataString);
+				resolve(dataString.trim());
 			} else {
-				reject(`Process exited with exit code ${code}`);
+				reject(errorString.trim());
 			}
 		});
 	});
