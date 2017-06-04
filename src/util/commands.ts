6fd307d7ecee107b5b4319e24b05a322f41f6605
drewbrokke <drew.brokke@liferay.com>
2017-06-04 01:16:26 -0700

    commands.ts - imports promisifyChildProcess function

--- src/util/commands.ts
+++ src/util/commands.ts
@@ -1,9 +1,10 @@
-import { ChildProcess, spawn } from 'child_process';
+import { spawn } from 'child_process';
 import * as clipboardy from 'clipboardy';
 import * as opn from 'opn';
 import * as path from 'path';
 
 import { notifyError, notifyInfo, notifySuccess } from '../interface/notification';
+import { promisifyChildProcess } from './promisify-child-process';
 
 let REPO_TOP_LEVEL: string;
 
@@ -51,25 +52,4 @@ function doOpenFilesFromCommit(SHA: string, repoTopLevel: string): void {
 
 function handleOpenFilesFromCommitError(errorMessage: string) {
 	notifyError(`Could not open files:\n\n${errorMessage}`);
-}
-
-function promisifyChildProcess(childProcess: ChildProcess): Promise<any> {
-	return new Promise((resolve, reject) => {
-		let dataString = '';
-		let errorString = '';
-
-		childProcess.stdout.setEncoding('utf8');
-		childProcess.stderr.setEncoding('utf8');
-
-		childProcess.stdout.on('data', (data: string) => dataString += data);
-		childProcess.stderr.on('data', (data: string) => errorString += data);
-
-		childProcess.on('close', (code: number) => {
-			if (code === 0) {
-				resolve(dataString.trim());
-			} else {
-				reject(errorString.trim());
-			}
-		});
-	});
 }
