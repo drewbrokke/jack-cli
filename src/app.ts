f7cca63528c2fec1b96ab058bb01045b02b657d5
drewbrokke <drew.brokke@liferay.com>
2017-06-05 12:01:58 -0700

    Prints help wanted message when piping data to jack

--- src/app.ts
+++ src/app.ts
@@ -23,5 +23,8 @@ export function run(args: string[]): void {
 }
 
 export function runFromPipedData(): void {
-	console.log('From Piped');
+	process.stdout.write('Piping into jack is not currently supported.\n');
+	process.stdout.write('If you would like to contribute or comment, please see the issue on GitHub at https://github.com/drewbrokke/jack/issues/9.\n');
+
+	process.exit(1);
 }
