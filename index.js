c6b81628de51dd6c8ea961e3ab6604cffc65dc4b
drewbrokke <drew.brokke@liferay.com>
2017-08-23 11:42:34 -0700

    this shouldn't be necessary anymore since we're handling the git log errors

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 #!/usr/bin/env node
 
-if (require('child_process').spawnSync('git', ['rev-parse', '--is-inside-work-tree']).status > 0) {
-	process.stderr.write('jack must be used inside a git project.\n');
-
-	process.exit(1);
-}
-
 const app = require('./dist/app');
 
 if (process.stdin.isTTY) {
