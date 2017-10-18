0458186945ec97750ded66c5fedc8111a645ecda
drewbrokke <drew.brokke@liferay.com>
2017-10-18 14:29:05 -0700

    commands-def - adds documentation comments for the placeholder variables

--- src/util/commands-def.ts
+++ src/util/commands-def.ts
@@ -12,9 +12,38 @@ export interface ICommand {
 	onErrorCommand?: string[] | null;
 }
 
+/**
+ * Will be replaced by the currently selected commit SHA.
+ *
+ * Single Commit Example:      4a22d67
+ * With Marked Commit Example: 4a22d67
+ */
 export const SHA_SINGLE_PLACEHOLDER = '[% SHA_SINGLE %]';
+
+/**
+ * Will always be replaced by a revision range, even if there is no marked
+ * commit. Commands such as 'git diff' require this to show the changes for just
+ * a single commit.
+ *
+ * Single Commit Example:      4a22d67^..4a22d67
+ * With Marked Commit Example: 9103ae0^..4a22d67
+ */
 export const SHA_RANGE_PLACEHOLDER = '[% SHA_RANGE %]';
+
+/**
+ * Will be replaced by either a single commit SHA or a revision range if there
+ * is a marked commit.
+ *
+ * Single Commit Example:      4a22d67
+ * With Marked Commit Example: 9103ae0^..4a22d67
+ */
 export const SHA_SINGLE_OR_RANGE_PLACEHOLDER = '[% SHA_SINGLE_OR_RANGE %]';
+
+/**
+ * Will be replaced by the commit message of the currently selected commit
+ *
+ * Example: "offers commit message variable for custom commands"
+ */
 export const COMMIT_MESSAGE_PLACEHOLDER = '[% COMMIT_MESSAGE %]';
 
 export const COMMANDS: ICommand[] = [
