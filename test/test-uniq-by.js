361d2b863ce607ed01b3b2de1f68a3a3a4ece17d
drewbrokke <drew.brokke@liferay.com>
2018-04-20 11:31:04 -0700

    adds test-uniq-by.js

---
+++ test/test-uniq-by.js
@@ -0,0 +1,21 @@
+import test from 'ava';
+
+import { uniqBy, uniqByLast } from '../dist/util/uniq-by';
+
+const objects = [
+	{ name: 'a', id: 1 },
+	{ name: 'b', id: 2 },
+	{ name: 'a', id: 3 },
+];
+
+const stringify = (objArray) => objArray.map((obj) => obj.id).join('');
+
+test('uniqBy', t => {
+	t.plan(1);
+	t.is(stringify(uniqBy(objects, 'name')), '12');
+});
+
+test('uniqByLast', async t => {
+	t.plan(1);
+	t.is(stringify(uniqByLast(objects, 'name')), '23');
+});
