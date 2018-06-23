5384da235f8f72d70f7f256ba7488620dca09e2b
drewbrokke <drew.brokke@liferay.com>
2018-06-22 17:30:39 -0700

    adds basic test cases for the util

---
+++ test/command-string-util.js
@@ -0,0 +1,35 @@
+import test from 'ava';
+
+import { stringToCommandArray } from '../dist/util/command-string-util';
+
+test('stringToCommandArray', t => {
+	t.plan(2);
+	t.deepEqual(
+		stringToCommandArray('hello world'),
+		['hello', 'world']);
+
+	t.deepEqual(
+		stringToCommandArray('git -p diff [%SHA_RANGE%] --name-only | sed s,/src/.*,, | sed s,/build.gradle,, | uniq -c | sort -r | less'),
+		[
+			"git",
+			"-p",
+			"diff",
+			"[%SHA_RANGE%]",
+			"--name-only",
+			"|",
+			"sed",
+			"s,/src/.*,,",
+			"|",
+			"sed",
+			"s,/build.gradle,,",
+			"|",
+			"uniq",
+			"-c",
+			"|",
+			"sort",
+			"-r",
+			"|",
+			"less"
+		]
+	);
+});
