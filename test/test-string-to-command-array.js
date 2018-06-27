c60a700b775a60a11039d4ff9067db0feb10fc96
drewbrokke <drew.brokke@liferay.com>
2018-06-27 08:35:02 -0700

    adds additional test cases for the command string parser

--- test/test-string-to-command-array.js
+++ test/test-string-to-command-array.js
@@ -3,10 +3,17 @@ import test from 'ava';
 import { stringToCommandArray } from '../dist/util/util-functions';
 
 test('stringToCommandArray', t => {
-	t.plan(2);
 	t.deepEqual(
 		stringToCommandArray('hello world'),
 		['hello', 'world']);
+
+	t.deepEqual(
+		stringToCommandArray('one "two three" four'),
+		['one', 'two three', 'four']);
+
+	t.deepEqual(
+		stringToCommandArray('one two "three four"'),
+		['one', 'two', 'three four']);
 
 	t.deepEqual(
 		stringToCommandArray('git -p diff [%SHA_RANGE%] --name-only | sed s,/src/.*,, | sed s,/build.gradle,, | uniq -c | sort -r | less'),
