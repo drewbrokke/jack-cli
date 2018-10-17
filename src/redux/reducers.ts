de1f42bf7a71c0cf42c6180e32744e7d551aff7f
drewbrokke <drew.brokke@liferay.com>
2018-10-17 14:37:16 -0700

    adds immer to the reducer to get immutability for free

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,3 +1,4 @@
+import { produce } from 'immer';
 import { ActionType, IAction, IState, Status, View } from '../types/types';
 
 const COMMIT_SHA_REGEX: RegExp = new RegExp(/[0-9a-f]{7,40}\b/);
@@ -14,73 +15,79 @@ const INITIAL_STATE: IState = {
 export const reducer = (
 	state: IState = INITIAL_STATE,
 	action: IAction,
-): IState => {
-	const {
-		index: currentIndex,
-		indexesWithSHAs: currentIndexesWithSHAs,
-		lines: currentLines,
-		SHA: currentSHA,
-	} = state;
+): IState =>
+	produce(state, (draft) => {
+		const {
+			index: currentIndex,
+			indexesWithSHAs: currentIndexesWithSHAs,
+			lines: currentLines,
+			SHA: currentSHA,
+		} = state;
 
-	switch (action.type) {
-		case ActionType.ADD_COMMITS:
-			const newLines = action.payload;
+		switch (action.type) {
+			case ActionType.ADD_COMMITS:
+				const newLines = action.payload;
 
-			const lines = [...currentLines, ...newLines];
+				const lines = [...currentLines, ...newLines];
 
-			const newIndexesWithSHAs: number[] = newLines
-				.map((line: string, index: number) => {
-					if (COMMIT_SHA_REGEX.test(line)) {
-						return index + currentLines.length;
-					}
+				const newIndexesWithSHAs: number[] = newLines
+					.map((line: string, index: number) => {
+						if (COMMIT_SHA_REGEX.test(line)) {
+							return index + currentLines.length;
+						}
 
-					return 0;
-				})
-				.filter(Boolean);
+						return 0;
+					})
+					.filter(Boolean);
 
-			const indexesWithSHAs: number[] = [
-				...currentIndexesWithSHAs,
-				...newIndexesWithSHAs,
-			];
+				const indexesWithSHAs: number[] = [
+					...currentIndexesWithSHAs,
+					...newIndexesWithSHAs,
+				];
 
-			const SHA = currentSHA
-				? currentSHA
-				: getSHA(indexesWithSHAs[currentIndex], lines, currentSHA);
+				const SHA = currentSHA
+					? currentSHA
+					: getSHA(indexesWithSHAs[currentIndex], lines, currentSHA);
 
-			return { ...state, SHA, indexesWithSHAs, lines };
+				draft.SHA = SHA;
+				draft.lines = lines;
+				draft.indexesWithSHAs = indexesWithSHAs;
 
-		case ActionType.CLEAR_LOG:
-			return INITIAL_STATE;
+				break;
 
-		case ActionType.MARK_SHA:
-			return { ...state, markedSHA: action.payload };
+			case ActionType.CLEAR_LOG:
+				Object.assign(draft, INITIAL_STATE);
+				break;
 
-		case ActionType.UPDATE_INDEX:
-			const newIndex = Math.min(
-				Math.max(currentIndex + action.payload, 0),
-				currentIndexesWithSHAs.length - 1,
-			);
+			case ActionType.MARK_SHA:
+				draft.markedSHA = action.payload;
+				break;
 
-			return {
-				...state,
-				SHA: getSHA(
+			case ActionType.UPDATE_INDEX:
+				const newIndex = Math.min(
+					Math.max(currentIndex + action.payload, 0),
+					currentIndexesWithSHAs.length - 1,
+				);
+
+				draft.SHA = getSHA(
 					currentIndexesWithSHAs[newIndex],
 					currentLines,
 					currentSHA,
-				),
-				index: newIndex,
-			};
+				);
 
-		case ActionType.UPDATE_STATUS:
-			return { ...state, status: action.payload };
+				draft.index = newIndex;
 
-		case ActionType.UPDATE_VIEW:
-			return { ...state, view: action.payload };
+				break;
 
-		default:
-			return state;
-	}
-};
+			case ActionType.UPDATE_STATUS:
+				draft.status = action.payload;
+				break;
+
+			case ActionType.UPDATE_VIEW:
+				draft.view = action.payload;
+				break;
+		}
+	});
 
 const getSHA = (
 	index: number,
