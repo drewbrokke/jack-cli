e31ff3ece2460d8d5c233d1ef7af68966fbc6b6a
drewbrokke <drew.brokke@liferay.com>
2018-12-17 16:04:40 -0800

    progress-indicator.ts - speed up retrieving the git log by only showing the total number of commits after the git log process is finished

--- src/interface/progress-indicator.ts
+++ src/interface/progress-indicator.ts
@@ -1,15 +1,23 @@
 import { doSubscribe } from '../redux/store';
-import { StateProperty, TextElement, UpdateFunction } from '../types/types';
+import {
+	StateProperty,
+	Status,
+	TextElement,
+	UpdateFunction,
+} from '../types/types';
 import { getTextElement } from './interface-elements';
+
+const TOTAL_PLACEHOLDER = '---';
 
 export const getProgressIndicator = (): TextElement => {
 	const progressIndicator: TextElement = getTextElement({
+		content: getStatusMessage(1, TOTAL_PLACEHOLDER),
 		left: 0,
 		name: 'progressIndicator',
 	});
 
 	doSubscribe(
-		[StateProperty.index, StateProperty.indexesWithSHAs],
+		[StateProperty.index, StateProperty.status],
 		progressIndicator,
 		updateProgressIndicator,
 	);
@@ -17,13 +25,23 @@ export const getProgressIndicator = (): TextElement => {
 	return progressIndicator;
 };
 
+const getStatusMessage = (
+	left: string | number,
+	right: string | number,
+): string => `Commit ${left} of ${right}`;
+
 const updateProgressIndicator: UpdateFunction<TextElement> = async ({
 	element: progressIndicator,
-	state,
+	state: { index, indexesWithSHAs, status },
 }) => {
-	const { index, indexesWithSHAs } = state;
-
-	progressIndicator.setText(`Commit ${index + 1}/${indexesWithSHAs.length}`);
+	progressIndicator.setText(
+		getStatusMessage(
+			index + 1,
+			status === Status.LOG_COMPLETED
+				? indexesWithSHAs.length
+				: TOTAL_PLACEHOLDER,
+		),
+	);
 
 	return true;
 };
