d10447d954381a1d17b069c443ca420f7ba8408c
drewbrokke <drew.brokke@liferay.com>
2020-06-09 11:48:46 -0500

    (colors.ts) fixes newlines not rendering properly by using 'kleur' instead of 'ansi-colors'

--- src/util/colors.ts
+++ src/util/colors.ts
@@ -1,12 +1,12 @@
-import * as c from 'ansi-colors';
+import * as kleur from 'kleur';
 
 export type ColorFn = (s: string) => string;
 
 export const colors = {
-	emphasis: (s: string) => c.bold(s),
-	error: (s: string) => c.bold.red(s),
-	info: (s: string) => c.bold.blue(s),
-	searchHit: (s: string) => c.inverse(s),
-	success: (s: string) => c.bold.green(s),
-	warning: (s: string) => c.bold.yellow(s),
+	emphasis: (s: string) => kleur.bold(s),
+	error: (s: string) => kleur.bold().red(s),
+	info: (s: string) => kleur.bold().blue(s),
+	searchHit: (s: string) => kleur.inverse(s),
+	success: (s: string) => kleur.bold().green(s),
+	warning: (s: string) => kleur.bold().yellow(s),
 };
